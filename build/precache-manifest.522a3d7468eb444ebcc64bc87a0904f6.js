1f43d46ecd1af3b3ecd1f74310fe6894d7da4285
SoapKe <soapke@outlook.com>
2018-12-03 22:08:42 -0500

    Config CI with pre-build files

---
+++ build/precache-manifest.522a3d7468eb444ebcc64bc87a0904f6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "4fe595c965653484d0fc",
+    "url": "./static/js/main.4fe595c9.chunk.js"
+  },
+  {
+    "revision": "6004cd008720dc9a93f6",
+    "url": "./static/js/1.6004cd00.chunk.js"
+  },
+  {
+    "revision": "6004cd008720dc9a93f6",
+    "url": "./static/css/1.cdc13be6.chunk.css"
+  },
+  {
+    "revision": "f7f83649b200156cc5fb838ef2112527",
+    "url": "./index.html"
+  }
+];
