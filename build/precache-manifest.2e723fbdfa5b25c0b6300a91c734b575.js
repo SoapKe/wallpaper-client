38c338a013b15fb27795f72493cd38815602ca6f
cdong1995 <cd3032@columbia.edu>
2018-12-12 23:40:36 -0500

    modify choose folder

---
+++ build/precache-manifest.2e723fbdfa5b25c0b6300a91c734b575.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "daef24025be01b0ed785",
+    "url": "./static/js/main.daef2402.chunk.js"
+  },
+  {
+    "revision": "5d7cdf530ebdee7035c5",
+    "url": "./static/js/1.5d7cdf53.chunk.js"
+  },
+  {
+    "revision": "5d7cdf530ebdee7035c5",
+    "url": "./static/css/1.a4f24ee4.chunk.css"
+  },
+  {
+    "revision": "9d483229879801273b5642c33ca16d95",
+    "url": "./index.html"
+  }
+];
