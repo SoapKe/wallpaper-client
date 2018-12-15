a90b860d26314cde1d9bbefada3d0d3b7de165dc
Spider-Min <mintianchen@126.com>
2018-12-14 23:28:09 -0500

    Delete trash

---
+++ build/precache-manifest.321ab769451c19820acb53b22a1f4e12.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "bb56879709ba7463e834",
+    "url": "./static/js/main.bb568797.chunk.js"
+  },
+  {
+    "revision": "f970e7aa51668a91bde2",
+    "url": "./static/js/1.f970e7aa.chunk.js"
+  },
+  {
+    "revision": "f970e7aa51668a91bde2",
+    "url": "./static/css/1.bd9bcac0.chunk.css"
+  },
+  {
+    "revision": "6f6d8c98554134dbaa853ce7449065ef",
+    "url": "./index.html"
+  }
+];
